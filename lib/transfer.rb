fbdb0ba8bb578218f7544eccae9c0cd73b104612
Joseph Senders <jasenders@gmail.com>
2018-08-27 22:27:13 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,30 @@
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+def valid?
+  sender.valid? && receiver.valid? && sender.balance >self.amount
+end
+
+def execute_transaction 
+  
+end
+
+def reverse_transfer 
+  if @status=="complete"
+      @status = "pending"
+      @amount = -@amount
+      execute_transaction
+      @status = "reversed"
+    end
+  end
+end
+
 end
